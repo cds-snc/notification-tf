f62f0b6123c770112cb5bf893fb42ad3819c998e
Max Neuvians <max@neuvians.net>
2020-01-23 12:53:14 -0500

    Added alerts

--- infrastructure/aws/build/buckets.tf
+++ infrastructure/aws/build/buckets.tf
@@ -33,3 +33,43 @@ resource "aws_s3_bucket" "document_bucket" {
     }
   }
 }
+
+resource "aws_s3_bucket" "alb_log_bucket" {
+  bucket = "${var.name}-log-bucket"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+data "aws_elb_service_account" "main" {}
+
+resource "aws_s3_bucket_policy" "notification" {
+  bucket = aws_s3_bucket.alb_log_bucket.id
+
+  policy = <<POLICY
+{
+  "Id": "notificationALBWrite",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "notificationALBWrite",
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.name}-log-bucket/alb/*",
+      "Principal": {
+        "AWS": [
+          "${data.aws_elb_service_account.main.arn}"
+        ]
+      }
+    }
+  ]
+}
+POLICY
+}
--- infrastructure/aws/build/sns.tf
+++ infrastructure/aws/build/sns.tf
@@ -3,9 +3,17 @@ resource "aws_sns_topic" "ses_callback" {
   name = "ses-callback"
 }
 
+resource "aws_sns_topic" "alert_warning" {
+  name = "alert-warning"
+}
+
+resource "aws_sns_topic" "alert_critical" {
+  name = "alert-critical"
+}
+
 #resource "aws_sns_topic_subscription" "ses_callback" {
 #  provider = aws.useast1
-#  topic_arn = "${aws_ses_topic.ses_callback.arn}"
+#  topic_arn = "${aws_sns_topic.ses_callback.arn}"
 #  protocol  = "https"
 #  endpoint  = "https://api.${var.domain}/notifications/email/ses"
 #}
